8f80e4a4211f8de5a702975dddfde6c851e746f2
Mohamed Yousef <136084083+mohamed1yousef@users.noreply.github.com>
2024-04-27 21:35:17 +0300

    Passive

---
+++ Passive_Battary_Balancing/Passive_Balance_types.h
@@ -0,0 +1,30 @@
+/*
+ * File: Passive_Balance_types.h
+ *
+ * Code generated for Simulink model 'Passive_Balance'.
+ *
+ * Model version                  : 1.14
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Sat Apr 27 20:27:26 2024
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: ARM Compatible->ARM Cortex
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_Passive_Balance_types_h_
+#define RTW_HEADER_Passive_Balance_types_h_
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_Passive_Balance_T RT_MODEL_Passive_Balance_T;
+
+#endif                                 /* RTW_HEADER_Passive_Balance_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
